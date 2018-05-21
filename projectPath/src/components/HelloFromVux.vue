710be1dad391faef70ccd87aa55bae069e4caee7
钱博康 <aa@Kaso.local>
2018-05-21 22:14:26 +0800

    cc

--- projectPath/src/components/HelloFromVux.vue
+++ projectPath/src/components/HelloFromVux.vue
@@ -1,41 +1,67 @@
 <template>
   <div>
-    <div class="vux-demo">
-      <img class="logo" src="../assets/vux_logo.png">
-      <h1> </h1>
-    </div>
-    <group title="cell demo">
-      <cell title="VUX" value="cool" is-link></cell>
-    </group>
+    <GroupTitle>更多功能</GroupTitle>
+    <Grid :cols="3">
+      <GridItem :label="i.label" v-for="i in GetPremisson" :key="i.length">
+        <font slot="icon" class="iconfont" :class="i.icon"></font>
+      </GridItem>
+    </Grid>
   </div>
 </template>
 
 <script>
-import { Group, Cell } from 'vux'
+  /* eslint-disable */
+  import {Grid, GridItem, GroupTitle} from 'vux'
 
-export default {
-  components: {
-    Group,
-    Cell
-  },
-  data () {
-    return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Hello World!'
+  export default {
+    components: {
+      Grid, GridItem,
+      GroupTitle
+    },
+    data() {
+      return {
+        access: 0,
+        permission: [{
+          gridList: [{
+            label: "到课率详表",
+            icon: "icon-touyingyi"
+          }, {
+            label: "缺课学生报表",
+            icon: "icon-touyingyi"
+          }]
+        }, {
+          gridList: [{
+            label: "辅导员分析",
+            icon: "icon-touyingyi"
+          }, {
+            label: "教师分析",
+            icon: "icon-touyingyi"
+          }, {
+            label: "课程分析分析",
+            icon: "icon-touyingyi"
+          }]
+        }, {
+          gridList: [{
+            label: "校园分析",
+            icon: "icon-touyingyi"
+          }, {
+            label: "系部分析",
+            icon: "icon-touyingyi"
+          }]
+        }]
+      }
+    },
+    computed: {
+      GetPremisson() {
+        let str = [];
+        for (let i = 0; i <= this.access; i++) {
+          str.push(...this.permission[i].gridList)
+        }
+        return str;
+      }
     }
   }
-}
 </script>
-
-<style>
-.vux-demo {
-  text-align: center;
-}
-.logo {
-  width: 100px;
-  height: 100px
-}
+<style scoped>
+  @import "http://at.alicdn.com/t/font_619315_w5z0epg7415g66r.css";
 </style>
