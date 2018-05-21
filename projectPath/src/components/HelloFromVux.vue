494e6ce41a3ea89cdd4b7b2f2f7266599cef0ca6
钱博康 <aa@Kaso.local>
2018-05-21 22:32:58 +0800

    c'c

--- projectPath/src/components/HelloFromVux.vue
+++ projectPath/src/components/HelloFromVux.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <GroupTitle>更多功能</GroupTitle>
-    <Grid :cols="3">
+    <Grid class="bgWhite" :cols="4" :show-lr-borders="false" :show-vertical-dividers="false">
       <GridItem :label="i.label" v-for="i in GetPremisson" :key="i.length">
-        <font slot="icon" class="iconfont" :class="i.icon"></font>
+        <font :color="i.color" slot="icon" class="iconfont" :class="i.icon"></font>
       </GridItem>
     </Grid>
   </div>
@@ -20,33 +20,40 @@
     },
     data() {
       return {
-        access: 0,
+        access: 2,
         permission: [{
           gridList: [{
             label: "到课率详表",
-            icon: "icon-touyingyi"
+            icon: "icon-renqun",
+            color: "#248888"
           }, {
             label: "缺课学生报表",
-            icon: "icon-touyingyi"
+            icon: "icon-bug",
+            color: "#E7475E"
           }]
         }, {
           gridList: [{
             label: "辅导员分析",
-            icon: "icon-touyingyi"
+            icon: "icon-gerenxinxiyebanzhurenlaoshi",
+            color: "#248888"
           }, {
             label: "教师分析",
-            icon: "icon-touyingyi"
+            icon: "icon-custom-teacher",
+            color: "#f07818"
           }, {
             label: "课程分析分析",
-            icon: "icon-touyingyi"
+            icon: "icon-xinrenkecheng",
+            color: "#248888"
           }]
         }, {
           gridList: [{
             label: "校园分析",
-            icon: "icon-touyingyi"
+            icon: "icon-xueyuan",
+            color: "#f07818"
           }, {
             label: "系部分析",
-            icon: "icon-touyingyi"
+            icon: "icon-huijihesuan",
+            color: "#248888"
           }]
         }]
       }
@@ -54,8 +61,10 @@
     computed: {
       GetPremisson() {
         let str = [];
-        for (let i = 0; i <= this.access; i++) {
-          str.push(...this.permission[i].gridList)
+        if (this.access != 3) {
+          for (let i = 0; i <= this.access; i++) {
+            str.push(...this.permission[i].gridList)
+          }
         }
         return str;
       }
@@ -63,5 +72,16 @@
   }
 </script>
 <style scoped>
-  @import "http://at.alicdn.com/t/font_619315_w5z0epg7415g66r.css";
+  @import "http://at.alicdn.com/t/font_675461_g11k10ytjaq93sor.css";
+
+  .iconfont {
+    font-size: 1.3em;
+  }
+
+  .bgWhite {
+    background: white;
+  }
+  .weui-grids:before,.weui-grid:after{
+    border: none!important;
+  }
 </style>
