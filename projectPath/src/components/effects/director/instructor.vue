a730ff9a3acb6f0032b56d0e90a697ccf9d1aba2
qianbokang <516764216@qq.com>
2018-06-07 19:06:18 +0800

    cc

--- projectPath/src/components/effects/director/instructor.vue
+++ projectPath/src/components/effects/director/instructor.vue
@@ -5,8 +5,8 @@
     </div>
     <checklist :style="button?'display:block':'display:none'" title="显示范围" :options="checklist.commonList"
                v-model="checklist.checklist"></checklist>
-    <chartCol :chart="chart"></chartCol>
     <chartLine :chart="chart2"></chartLine>
+    <chartCol :chart="chart"></chartCol>
     <chartCompare :chart="chart3"></chartCompare>
   </div>
 </template>
@@ -31,7 +31,7 @@
       return {
         chart2: {
           config: {
-            title: '本周的测试标题',
+            title: '上周信息系签到率报表',
             max: 100,
             min: 60
           },
@@ -47,7 +47,7 @@
         },
         chart3: {
           config: {
-            title: "本周信息系签到率"
+            title: "上周辅导员管辖班级签到率对比"
           },
           data: [
             {content: "甘小武", y: 65, x: 1940},
@@ -81,14 +81,14 @@
         },
         chart: {
           config: {
-            title: '辅导员管理班级签到率'
+            title: '上周辅导员平均签到率'
           },
           data: [
-            {x: '信息系', y: 75},
-            {x: '法律系', y: 85},
-            {x: '外语系', y: 90},
-            {x: '会计系', y: 78},
-            {x: '金融系', y: 93},
+            {x: '甘老师', y: 75},
+            {x: '张老师', y: 85},
+            {x: '刘老师', y: 90},
+            {x: '李老师', y: 78},
+            {x: '米老师', y: 93},
           ]
 
         },
